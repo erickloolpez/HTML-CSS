9f95765e47b01b4a82b93ae8e7ad56bd6478b4ed
erickloolpez <erickloopeze@hotmail.com>
2024-09-18 15:03:09 -0500

    Adding the portfolyo

---
+++ portfolio/app.js
@@ -0,0 +1,11 @@
+//sticky navbar
+
+const navbar = document.querySelector('nav')
+
+window.addEventListener('scroll', ()=>{
+    if (scrollY > 30) {
+        navbar.classList.add('active')
+    }else{
+        navbar.classList.remove('active')
+    }
+})
