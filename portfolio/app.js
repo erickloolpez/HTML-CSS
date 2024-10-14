b4fcc81322b6f622eb5e771f81ebb6155977caa1
erickloolpez <erickloopeze@hotmail.com>
2024-10-13 22:13:33 -0500

    Portofolio complete

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,26 +1,40 @@
 //sticky navbar
 
 const navbar = document.querySelector('nav')
+const navbarUl = document.querySelector('nav ul')
 const themeheading = document.querySelector('.theme-colors h2')
 
-window.addEventListener('scroll', ()=>{
-    if (scrollY > 30) {
+let isMobile = false
+
+window.addEventListener('scroll', () => {
+    if (scrollY > 30 && !isMobile) {
         navbar.classList.add('active')
         themeheading.classList.add('active')
-    }else{
+        navbarUl.style.display = "flex"
+    } else {
         navbar.classList.remove('active')
         themeheading.classList.remove('active')
+        navbarUl.style.display = "none"
     }
 })
+
+window.addEventListener('resize', function () {
+    if (window.innerWidth <= 768) {
+        isMobile = true
+        navbarUl.style.display = "none"
+    } else {
+        isMobile = false
+    }
+});
 
 //scroll top bar
 
 const scrolltotop = document.querySelector('.scrolltop a')
 
-window.addEventListener('scroll',()=>{
-    if(scrollY > 1000){
+window.addEventListener('scroll', () => {
+    if (scrollY > 1000) {
         scrolltotop.classList.add('active')
-    }else{
+    } else {
         scrolltotop.classList.remove('active')
     }
 })
@@ -30,40 +44,40 @@ window.addEventListener('scroll',()=>{
 const moon = document.querySelector('.bx-moon')
 const sun = document.querySelector('.bx-sun')
 
-moon.addEventListener('click',()=>{
+moon.addEventListener('click', () => {
     document.body.classList.add('darkmode')
     moon.style.display = 'none'
-    sun.style.display = 'block'
+    sun.style.display = 'inline-block'
 })
-sun.addEventListener('click',()=>{
+sun.addEventListener('click', () => {
     document.body.classList.remove('darkmode')
     sun.style.display = 'none'
-    moon.style.display = 'block'
+    moon.style.display = 'inline-block'
 })
 
 //color picker
 const colorpicker = document.querySelectorAll('.colorpicker')
 
-colorpicker[0].addEventListener('click',()=>{ 
+colorpicker[0].addEventListener('click', () => {
     document.body.classList.add('clr1')
-    document.body.classList.remove('clr2','clr3','clr4')
+    document.body.classList.remove('clr2', 'clr3', 'clr4')
 })
-colorpicker[1].addEventListener('click',()=>{ 
+colorpicker[1].addEventListener('click', () => {
     document.body.classList.add('clr2')
-    document.body.classList.remove('clr1','clr3','clr4')
+    document.body.classList.remove('clr1', 'clr3', 'clr4')
 })
-colorpicker[2].addEventListener('click',()=>{ 
+colorpicker[2].addEventListener('click', () => {
     document.body.classList.add('clr3')
-    document.body.classList.remove('clr2','clr1','clr4')
+    document.body.classList.remove('clr2', 'clr1', 'clr4')
 })
-colorpicker[3].addEventListener('click',()=>{ 
+colorpicker[3].addEventListener('click', () => {
     document.body.classList.add('clr4')
-    document.body.classList.remove('clr2','clr3','clr1')
+    document.body.classList.remove('clr2', 'clr3', 'clr1')
 })
 
 //show/hide toggle nav list-items for responsive layout
-const listitems= document.querySelector('nav ul')
+const listitems = document.querySelector('nav ul')
 
-function listItems(){
+function listItems() {
     listitems.classList.toggle('listitems')
 }
